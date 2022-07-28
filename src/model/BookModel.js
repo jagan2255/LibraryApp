ee4d37dd0305f39700b9c2a114df6e068981bc25
jagan2255 <101348715+jagan2255@users.noreply.github.com>
2022-07-28 12:55:22 +0530

    Update BookModel.js

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:user123@project1.cfkyt.mongodb.net/Library?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/Library', {useNewUrlParser: true, useUnifiedTopology: true});
 const Schema = mongoose.Schema;
 
 
